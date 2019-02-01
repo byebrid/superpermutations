2eec6fc53f159e0c0b05cf755318a6616a3b4c67
byebrid <lexlmgallon@gmail.com>
2019-02-01 11:41:39 +1100

    Update superpermutations.py

--- superpermutations.py
+++ superpermutations.py
@@ -1,82 +1,130 @@
-"""Created by Byebrid. hamilton() will definitely not necessarily find shortest
-path. This means this script can be used to provide an upper bound but not find
-the smallest possible superpermutation for any sets of size > 4.
-"""
-
-from itertools import permutations
-
-string = 'ABCDEF'
-
-# Setting up array of all permutations of string
-perms = [''.join(p) for p in permutations(string)]
+"""Created by Byebrid."""
+from itertools import permutations, combinations
+from math import factorial
 
 
-class Node:
-    """A Node is some permutation of the desired letters."""
+def bounds(alphabet):
+    """Finds lower & upper bounds of superperumtation length of given alphabet.
+    See OEIS to see where these formulae came from.
+    """
 
-    def __init__(self, value):
-        self.value = value
-        self.childs = set([])
+    n = len(alphabet)
 
-    def __repr__(self):
-        return "Node:" + self.value
+    lower_bound = factorial(n) + factorial(n - 1) + factorial(n - 2) + n - 3
 
-    def find_childs(self, group):
-        """Adds tuple of (child, weight) for each other node in the list of
-        nodes.
-        """
-        self.childs = set([])
-        for other in group:
-            if other == self:
+    if n >= 7:
+        upper_bound = (factorial(n) + factorial(n - 1) + factorial(n - 2)
+                       + factorial(n - 3) + n - 3)
+    else:
+        upper_bound = 0
+        for k in range(1, n + 1):
+            p = len(list(permutations(alphabet, k)))
+            c = len(list(combinations(alphabet, k)))
+            upper_bound += p / c
+
+    return (lower_bound, upper_bound)
+
+
+def dist_btwn(self, other):
+    """Finds 'distance' between 2 nodes."""
+    for d, char in enumerate(self):
+        self_end = self[d:]
+        if other.startswith(self_end):
+            dist = len(other) - len(self_end)
+            return dist
+
+    # If end of self does not begin other, then dist would just be len(other)
+    return len(other)
+
+
+def concatenate(path):
+    """Given path, will concatenate all terms in path.
+
+    Example: >>> concatenate(['ACB', 'CBA', 'BCA'])
+             ACBABCA
+
+    """
+    print("Concatenating ", path)
+
+    # Result starts off with first word
+    result = path[0]
+
+    for i, word in enumerate(path):
+        if word == path[-1]:
+            return result
+
+        next_word = path[i + 1]
+
+        for j in range(len(word)):
+            word_end = word[j:]
+            if next_word.startswith(word_end):
+                # Number of characters we need to remove from start of next_word
+                # because they're already at end of word.
+                dist = len(word_end)
+                result += next_word[dist:]
+                break
+            elif j == len(word) - 1:
+                result += next_word
+
+
+def not_in_path(perms, path):
+    """Will return list of all elements of graph that are not already in path"""
+    L = []
+    for node in perms:
+        if not node in path:
+            L.append(node)
+    return L
+
+
+def hamilton(perms, pt=None, path=None, length=None):
+    """Designed with superpermutations in mind. Will recursively go through all
+    possible combinations of permutations of the alphabet (i.e. ABC, ACB, etc.).
+    If length becomes too great (assuming a superperm with length = lower_bound)
+    will always be possible), then goes no fruther down that path.
+    """
+
+    '''If this is the original call of hamilton(), loop through all the initial
+    nodes.'''
+    if pt is None:
+        # Goes through first layer of tree.
+        for s in perms:
+            print("Using {} as root node".format(s))
+            H = hamilton(perms, pt=s, path=[s], length=len(s))
+            # If H does not return a successful path, then just keep going
+            if H is None:
                 continue
-            for index in range(len(self.value)):
-                # If last portion of self is same as beginning of other:
-                if self.value[index] == other.value[0] and self.value[index:] in other.value:
-                    # Append the Node object and index (which = the 'distance')
-                    self.childs.add((other, index))
+            answer = concatenate(H)
+            return "Length: {}. Result is {}".format(len(answer), answer)
+        # If we didn't get any result from any path, panic!
+        return "NOTHING FOUND!"
 
+    unvisited = not_in_path(perms, path)
 
-# Create all 'words' as Node objects
-nodes = [Node(p) for p in perms]
-
-
-def hamilton(group, pt, path=[]):
-    """group is constantly updated as visited nodes are removed from group."""
-    # print("\ngroup = {}, pt = {}, path = {}".format(group, pt, path))
-    path.append(pt)
-    # If last element of group has been reached, return path.
-    if len(group) == 1:
+    '''Identify if superpermutation has become too long.  Else, see if we have
+    successfully found superpermutation of length lower_bound.'''
+    if length > lower_bound:
+        return None
+    if len(unvisited) == 0:
         return path
-    pt.find_childs(group)
-    dists = [child[1] for child in pt.childs]
-    shortest_dist = min(dists)
-    # First child that is shortest distance away is chosen, rest are ignored.
-    for child, dist in pt.childs:
-        if dist == shortest_dist:
-            # Remove current point from group so it is not used as a child in
-            # next iteration
-            group.discard(pt)
-            return hamilton(group=group, pt=child, path=path)
 
+    for child in unvisited:
+        new_path = path + [child]
+        new_length = length + dist_btwn(pt, child)
 
-shortest_len = 1000000
-for node in nodes:
-    # print("USING: ", node)
-    L = hamilton(group=set(nodes), pt=node, path=[])
-    final_result = L[0].value
+        H = hamilton(perms=perms, pt=child,
+                     path=new_path, length=new_length)
 
-    # Concatenates all the permutations together.
-    for i, word in enumerate(L):
-        current_word = word.value
-        if i == len(L) - 1:
-            if len(final_result) < shortest_len:
-                shortest_len = len(final_result)
-                shortest_res = final_result
+        if H is None:
+            continue
         else:
-            next_word = L[i + 1].value
-            for j in range(len(next_word)):
-                if current_word[j] == next_word[0]:
-                    final_result += next_word[len(next_word) - j:]
+            return H
 
-print("Final result is of length {} and = {}".format(
-    len(shortest_res), shortest_res))
+
+alphabet = "ABC"
+lower_bound, upper_bound = bounds(alphabet)
+print("Length must be between {} and {}".format(lower_bound, upper_bound))
+
+# Finding all permutations of alphabet.
+perms = [''.join(p) for p in permutations(alphabet)]
+
+print(hamilton(perms=perms))
